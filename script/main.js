b9a7adb3ab14dd014caacbf97ffa2d2b8d6aa8b9
afzalpatel662 <afzalpatel662@gmail.com>
2024-12-10 12:15:58 +0000

    msg

--- script/main.js
+++ script/main.js
@@ -5,7 +5,23 @@ require(["DS/DataDragAndDrop/DataDragAndDrop", "DS/PlatformAPI/PlatformAPI", "DS
 			widgetDataSelected: {},
 	
 			onLoad: function() { 
-                widget.body.innerHTML = "Hello Afzal"
+                //widget.body.innerHTML = "Hello Afzal"
+                // Create a dropbox for drag-and-drop functionality
+				var dropbox = widget.createElement('div', { 'class' : 'mydropclass', 'text' : '' });
+				//var dropimage = widget.createElement('img', { 'src': 'https://krishnaprasadarisetty.github.io/SLK_Boss_ATT/BO_ATT/Images/dropImage.png', 'alt': 'Dropbox Image' });
+				//dropbox.append(dropimage);
+				var dropboxsep = widget.createElement('div', { 'class' : 'dropboxsep', 'text' : '-- or --' });
+				dropboxsep.style= "font-size: 12px; color: #d5e8f2; text-align: center";
+				dropbox.append(dropboxsep);
+				//var button = document.createElement('button', {'class':'dynamic-button'});
+
+				//button.style = "padding: 10px 20px; font-size: 14px; text-align: center; margin: 10px; background-color: #368ec4; color: white; border: none; cursor: pointer";
+				//button.innerHTML = 'Open content';
+				//dropbox.append(button);
+				dropbox.style = "border:2px #c6c5c5 dashed; margin:10px; padding: 5%; text-align: center";
+				widget.body.innerHTML="";
+				dropbox.inject(widget.body);
+				//
             }
 				
 		};
